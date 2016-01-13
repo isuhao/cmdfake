db035b1dd9a55e4f7dc5acd09b6e6a8d47432a88
邓景 <dengjing@dengjingdeMacBook-Pro.local>
2016-01-13 19:04:07 +0800

    基本样子，回头看内部的  x\y轴的自适应

--- client/templates/mark/content.js
+++ client/templates/mark/content.js
@@ -10,7 +10,7 @@ Template.content.onCreated(
 
 Template.content.helpers({
     content: () => {
-        return marked(Session.get('text'));
+        return marked(Session.get('text')) + '<div id="msg_end"></div>';
     }
 });
 
@@ -22,5 +22,12 @@ Template.content.events({
             console.log('!!');
         }
         Session.set('text', $textarea.context.innerText);
+        //var cul_height = $(e.target).find('#msg_end').context.offsetHeight + 20;
+        //console.log($(e.target).find('#msg_end'));
+        //console.log($(e.target).find('#msg_end'));
+        //t.$('#msg_end').scrollTop($textarea.context.scrollHeight);
+        //$(e.target).find('#msg_end').scrollTop();
+        //$(e.target).find('#msg_end').scrollIntoView();
+        //t.$('.display').scrollTop(cul_height);
     },
 });
